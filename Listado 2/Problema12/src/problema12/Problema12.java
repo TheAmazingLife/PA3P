ec403ec16bb407540c60bb352d3dafbb76f71f1c
Joaquin <102681497+TheAmazingLife@users.noreply.github.com>
2022-09-03 17:46:11 -0400

    Update Problema12.java

--- Listado 2/Problema12/src/problema12/Problema12.java
+++ Listado 2/Problema12/src/problema12/Problema12.java
@@ -3,5 +3,25 @@ package problema12;
 public class Problema12 {
 
     public static void main(String[] args) {
+        Persona persona = new Persona("textoDelString", true);
+        boolean estado = persona.esCasado();
+        String nombre = persona.elNombre();
+    }
+}
+
+class Persona {
+    private String nombre;
+    private boolean estado;
+
+    public Persona (String name, boolean est) {
+        nombre = name;
+        estado = est;
+    }
+    public boolean esCasado() {
+        return estado;
+    }
+    public String elNombre() {
+        String s = nombre;
+        return s;
     }
 }
