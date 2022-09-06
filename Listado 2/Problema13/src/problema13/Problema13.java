c6f9617406e6b3cae0d308e66cc4216845685a96
Joaquin San Martin <joaquin.san.vargas19@gmail.com>
2022-09-06 00:53:46 -0300

    Ejercicio 13 y 14

---
+++ Listado 2/Problema13/src/problema13/Problema13.java
@@ -0,0 +1,31 @@
+package problema13;
+
+class Problema13  {
+    public static void main(String[] args) {
+        Bebida vaso = new Bebida(true);
+        String s = vaso.beber();
+        System.out.println(s);
+        vaso = new Bebida(false);
+        s = vaso.beber();
+        System.out.println(s);
+    }
+}
+
+class Bebida {
+    private boolean llena;
+
+    public Bebida(boolean aux) {
+        llena = aux;
+    }
+
+    public String beber() {
+        String s;
+        if (llena) {
+            s = new String("gluglu");
+            llena = false;
+        } else {
+            s = null;
+        }
+        return s;
+    }
+}
